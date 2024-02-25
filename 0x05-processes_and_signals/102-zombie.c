1baa664496428dacb3898da04efc8b6c24fd2da4
AnasMouak <anasmouak1111@gmail.com>
2024-02-25 08:10:03 -0800

    zombie5

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -6,61 +6,61 @@
 int create_zombie(void);
 int infinite_while(void);
 
-/** 
+/**
  * infinite_while - function to continuously create zombie processes
- * 
+ *
  * Return: Always 0 (success)
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        create_zombie();
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
-/** 
+/**
  * create_zombie - function that create zombie processes
- * 
+ *
  * Return: Always 0 (success)
  */
 
-int create_zombie() 
+int create_zombie(void)
 {
-    pid_t child_pid;
+	pid_t child_pid;
+	int count = 0;
 
-    int count = 0;
-    while(count < 5)
-    {
-        child_pid = fork();
+	while (count < 5)
+	{
+		child_pid = fork();
 
-        if (child_pid > 0) 
-        {
-            printf("Zombie process created, PID: %d\n", child_pid);
-        } 
-        else if (child_pid == 0) 
-        {
-            exit(0);
-        } 
-        else 
-        {
-            fprintf(stderr, "Fork failed\n");
-            return (1);
-        }
-        count++;
-    }
-    return (0);
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+		}
+		else if (child_pid == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			fprintf(stderr, "Fork failed\n");
+			return (1);
+		}
+		count++;
+	}
+	return (0);
 }
 
-/** 
+/**
  * main - the entry to a program that creates 5 zombie processes
- * 
+ *
  * Return: Always 0 (success)
  */
 int main(void)
-{ 
-    infinite_while();
-    return (0);
+{
+	create_zombie();
+	infinite_while();
+	return (0);
 }
