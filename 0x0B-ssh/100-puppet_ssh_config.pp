3e6d992396a8ef20de0201d69b95a47dadedc327
AnasMouak <anasmouak1111@gmail.com>
2024-04-21 16:07:13 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,13 @@ include stdlib
 file_line { 'Turn off passwd auth':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  content => "PasswordAuthentication no\n",
+  content => "PasswordAuthentication no",
+  replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  content => "IdentityFile ~/.ssh/school\n",
+  content => "IdentityFile ~/.ssh/school",
+  replace => true,
 }
