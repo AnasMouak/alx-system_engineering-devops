5fe9fc28868a28709d6520cedce8ade815e58a1b
AnasMouak <anasmouak1111@gmail.com>
2024-04-23 08:03:52 -0700

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,6 @@
 # configuring server with Puppet
-package { 'nginx':
-  ensure   => '1.18.0',
-  provider => 'apt',
-}
 
-file { 'Hello World':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World',
-}
-
-file_line { 'Hello World':
-  path  => '/etc/nginx/sites-available/default',
-  after => 'server_name _;',
-  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-exec { 'service':
-  command  => 'service nginx start',
-  provider => 'shell',
-  user     => 'root',
-  path     => '/usr/sbin/service',
+exec {'install':
+  provider => shell,
+  command  =>' apt-get -y update ; apt-get -y install nginx ; ufw allow "Nginx HTTP"; echo "Hello World!" > /var/www/html/index.nginx-debian.html ;  service nginx start; sed -i "s,server_name _;,server_name _;\n\n\tlocation = /redirect_me/ {\n\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}," "/etc/nginx/sites-available/default"; service nginx restart',
 }
