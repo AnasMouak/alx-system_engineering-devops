b3f368152bea46eb6637344a25055df6a6fa535f
AnasMouak <anasmouak1111@gmail.com>
2024-06-06 06:12:03 -0700

    debugg

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # configuring server with Puppet
 
 exec {'fix':
-  command  =>' sed -i "s,phpp,php," "/var/www/html/wp-settings.php"; sed -i "s,phpp,php," "/var/www/html/zxcvbn.min.js",
+  command  =>' sed -i "s/phpp/php/g" "/var/www/html/wp-settings.php"; sed -i "s/phpp/php/g" "/var/www/html/zxcvbn.min.js",
   path     =>'/usr/local/bin/:/bin/'
 }
