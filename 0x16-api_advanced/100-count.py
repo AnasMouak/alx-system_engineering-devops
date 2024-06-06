7a4413f7fa99d5f7e221d55895c2c8a3534f1393
AnasMouak <anasmouak1111@gmail.com>
2024-06-06 04:53:09 -0700

    Update

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -24,7 +24,7 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
     headers = {"User-Agent": "Mozilla/5.0"}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
